31fff65f720d1d253d1bb5a6a6a7e9fa8a19b8b8
Tobias Luzuriaga <TOLU@novax.dk>
2024-09-13 12:09:10 +0200

    Minor Adjustment: Cursor pointer on sidebar li elements

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'shared-sidebar',
   templateUrl: './sidebar.component.html',
-  styles: ``
+  styles: `li {
+    cursor: pointer;
+  }`
 })
 export class SidebarComponent {
 
